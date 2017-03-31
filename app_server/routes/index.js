37957142329ce674b28f3c1007cfb918c8802901
Chris Johnson <csjohnson79@icloud.com>
2017-03-31 17:16:20 -0600

    created others.js, starting to build routes

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,7 +1,19 @@
 
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+// var ctrlMain = require('../controllers/main');
+var ctrlLocations = require('../controllers/locations.js');
+var ctrlOthers = require('../controllers/others.js');
+
+
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* Other pages */
+router.get('/about', ctrlOthers.about);
+
 
 /*
 router.get('/', function(req, res, next) {
@@ -11,6 +23,12 @@ router.get('/', function(req, res, next) {
 
 /* GET home page. */
 router.get('/', ctrlMain.index);
+
+/* GET 'about' page */
+module.exports.about = function(req, res){
+res.render('index', { title: 'About' });
+};
+
 
 
 module.exports = router;
